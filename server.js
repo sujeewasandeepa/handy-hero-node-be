972f5c11a94867fd8890b2c8dde819e1f48e011e
Sujeewa Sandeepa <sujeewasandeepa@protonmail.ch>
2023-05-16 18:57:57 +0530

    initial setup

--- server.js
+++ server.js
@@ -1,8 +1,2 @@
-const express = require("express");
-
-const app = express();
-const PORT = 4000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-})
+"use strict";
+//# sourceMappingURL=server.js.map
